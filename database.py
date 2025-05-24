6711b17579c17fbebef3a402d76f1e6ada06b1f6
puneeth <puneeth@forushealth.com>
2025-05-24 20:49:23 +0530

    database update

--- database.py
+++ database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 # SQLALCHEMY_DATABASE_URL =  "postgresql://postgres:punith%401903.@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://my_first_database_845y_user:G5zvg6k71cNBJvjjVjd1GTVNcqMkUF2R@dpg-d0otfm0dl3ps73aa5bo0-a.singapore-postgres.render.com/my_first_database_845y"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
